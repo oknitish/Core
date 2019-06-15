fd6f2a5bf578d205f0abc213a59d36718baa677e
srinivas571 <srinivas.cse5711@gmail.com>
2019-06-15 18:57:07 +0530

    very basic example with PreparedStatement

--- src/com/java/main/jdbc/statements/BasicExample.java
+++ src/com/java/main/jdbc/statements/BasicExample.java
@@ -2,6 +2,7 @@ package com.java.main.jdbc.statements;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class BasicExample {
@@ -14,20 +15,24 @@ public class BasicExample {
 		final String password = "system";
 
 		Connection con = null;
-
-		Statement stmt = null;
+		PreparedStatement stmt = null;
 
 		try {
 			Class.forName(driverClass);
 			con = DriverManager.getConnection(url, username, password);
+			String sql = "INSERT INTO CUSTOMERS VALUES (?,?,?,?,?)";
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, "7777");
+			stmt.setString(2, "stephen");
+			stmt.setString(3, "9988776655");
+			stmt.setString(4, "support@gmail.com");
+			stmt.setString(5, "india");
 
-			stmt = con.createStatement();
-
-			String sql = "INSERT INTO CUSTOMERS(custid,custname,custmobile,custemail,custaddress) VALUES ('7979','coresrinivas','6363952624','sri@gmail.com','HSR')";
-
-			int updatedrows = stmt.executeUpdate(sql);
+			int updatedrows = stmt.executeUpdate();
 
 			System.out.println("no of records updated:" + updatedrows);
+			stmt.close();
+			con.close();
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
